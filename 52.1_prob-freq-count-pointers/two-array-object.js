ebe8080cc6dd2f51d3c08b4fb9616099afaeb25c
David Bohman <david.bohman.dev@gmail.com>
2025-08-10 23:09:11 -0600

    write twoArrayObject fn

--- 52.1_prob-freq-count-pointers/two-array-object.js
+++ 52.1_prob-freq-count-pointers/two-array-object.js
@@ -1,3 +1,10 @@
-// add whatever parameters you deem necessary
-function twoArrayObject() {
+/** Takes two arrays, one of keys, one of values.
+ * Ignore values left over after all keys are assigned a value.
+ * Set value to null for any keys left over once all values are used.
+ */
+function twoArrayObject(keysArr, valuesArr) {
+    return keysArr.reduce((obj, key, idx) => {
+        obj[key] = idx < valuesArr.length ? valuesArr[idx] : null;
+        return obj;
+    }, {});
 }
