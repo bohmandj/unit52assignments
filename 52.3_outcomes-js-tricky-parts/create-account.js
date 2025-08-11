e7851d8f5b272df386b0949da3227bb2938a9c85
David Bohman <david.bohman.dev@gmail.com>
2025-08-11 11:00:12 -0600

    add createAccount fn w/ inner fns & lexical env closure

--- 52.3_outcomes-js-tricky-parts/create-account.js
+++ 52.3_outcomes-js-tricky-parts/create-account.js
@@ -1,5 +1,35 @@
 function createAccount(pin, amount) {
+    let currentPin = pin;
+    let balance = amount || 0;
 
+    function checkPin(inputPin) {
+        return inputPin === currentPin;
+    }
+
+    return {
+        checkBalance(inputPin) {
+            if (!checkPin(inputPin)) return "Invalid PIN.";
+            return `$${balance}`;
+        },
+        deposit(inputPin, depositAmount) {
+            if (!checkPin(inputPin)) return "Invalid PIN.";
+            balance += depositAmount;
+            return `Successfully deposited $${depositAmount}. Current balance: $${balance}.`;
+        },
+        withdraw(inputPin, withdrawAmount) {
+            if (!checkPin(inputPin)) return "Invalid PIN.";
+            if (withdrawAmount > balance) {
+                return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+            }
+            balance -= withdrawAmount;
+            return `Successfully withdrew $${withdrawAmount}. Current balance: $${balance}.`;
+        },
+        changePin(oldPin, newPin) {
+            if (!checkPin(oldPin)) return "Invalid PIN.";
+            currentPin = newPin;
+            return "PIN successfully changed!";
+        }
+    }
 }
 
 module.exports = { createAccount };
