455ad521c003d8230c8b217bccbccb3676100d46
David Bohman <david.bohman.dev@gmail.com>
2025-08-11 10:49:04 -0600

    add guessingGame fn w/ inner fn & lexical env closure

--- 52.3_outcomes-js-tricky-parts/guessing-game.js
+++ 52.3_outcomes-js-tricky-parts/guessing-game.js
@@ -1,5 +1,22 @@
 function guessingGame() {
+    const answer = Math.floor(Math.random() * 100);
+    let guesses = 0;
+    let isOver = false;
 
+    return function (guess) {
+        if (isOver) return `The game is over, you already won!`;
+
+        guesses++;
+
+        if (guess === answer) {
+            isOver = true;
+            return `You win! You found ${answer} in ${guesses} guess${guesses > 1 ? "es" : ""}.`;
+        } else if (guess < answer) {
+            return `${guess} is too low!`;
+        } else {
+            return `${guess} is too high!`;
+        }
+    }
 }
 
 module.exports = { guessingGame };
