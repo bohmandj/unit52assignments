e08fd7403fcb8b16d0878c41ce09a380f9f14fd8
David Bohman <david.bohman.dev@gmail.com>
2025-08-11 11:17:59 -0600

    add curriedAdd fn w/ inner fn & lexical env closure

--- 52.3_outcomes-js-tricky-parts/curried-add.js
+++ 52.3_outcomes-js-tricky-parts/curried-add.js
@@ -1,5 +1,13 @@
-function curriedAdd(total) {
+function curriedAdd(num) {
+    if (num === undefined) return 0;
+    let total = num;
 
+    function adder(nextNum) {
+        if (nextNum === undefined) return total;
+        total += nextNum;
+        return adder;
+    }
+    return adder;
 }
 
 module.exports = { curriedAdd };
