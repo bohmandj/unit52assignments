c440975e71661b154b3586e2d1aac631b6531099
David Bohman <david.bohman.dev@gmail.com>
2025-08-11 00:26:37 -0600

    write countPairs fn

--- 52.1_prob-freq-count-pointers/count-pairs.js
+++ 52.1_prob-freq-count-pointers/count-pairs.js
@@ -1,3 +1,18 @@
-// add whatever parameters you deem necessary
-function countPairs() {
+/** Takes array of integers and a target number.
+ *  Iterate through the array, adding each to a 'seen' set.
+ *  While iterating through, check if target - current num = a number we've seen:
+ *    - yes? add 1 to count
+ *    - no? add num to seen to check it against future numbers
+ *  Return number of pairs that add to the target sum.
+ */
+function countPairs(arr, target) {
+    const seen = new Set();
+    let count = 0;
+
+    for (let num of arr) {
+        let complement = target - num;
+        if (seen.has(complement)) count++;
+        seen.add(num);
+    }
+    return count;
 }
